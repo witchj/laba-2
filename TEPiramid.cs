85f0f48961938ec69b0120a2dabd564fdad1b784
witchj <61426856+witchj@users.noreply.github.com>
2020-11-11 12:37:33 +0200

    laba 2 task 2, var 4

---
+++ TEPiramid.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace labrab
+{
+    class TEPiramid : TETriangle
+    {
+        private uint _height;
+
+        public uint Height
+        {
+            get => _height;
+            set
+            {
+                _height = value;
+            }
+        }
+
+        public TEPiramid()
+        {
+
+        }
+        public TEPiramid(uint edgeLength, uint height)
+        {
+            EdgeLength = edgeLength;
+            Height = height;
+        }
+        public TEPiramid(TEPiramid piramid)
+        {
+            EdgeLength = piramid.EdgeLength;
+            Height = piramid.Height;
+        }
+
+        public override void OutputFields()
+        {
+            Console.WriteLine($"Edge Length = {EdgeLength}\nHeight = {Height}");
+        }
+
+        public override void InputFields()
+        {
+            bool isWrong = true;
+            uint tmp;
+            // Check for incorrect numbers in edge length
+            while (isWrong)
+            {
+                Console.WriteLine("Input Edge Length:");
+                tmp = Convert.ToUInt32(Console.ReadLine());
+
+                if (tmp > 0)
+                {
+                    EdgeLength = tmp;
+                    isWrong = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect number. Try again. ");
+                }
+            }
+            
+            isWrong = true;
+            // Check for incorrect numbers in height
+            while (isWrong)
+            {
+                Console.WriteLine("Input Height:");
+                tmp = Convert.ToUInt32(Console.ReadLine());
+
+                if (tmp > 0)
+                {
+                    Height = tmp;
+                    isWrong = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect number. Try again. ");
+                }
+            }
+        }
+
+        public void FindVolume()
+        {
+            double result;
+            // V = (h * pow(a,2)) / (4 * √3)
+            // h - height, a - EdgeLength
+            result = (Height * Math.Pow(EdgeLength, 2)) / (4 * Math.Sqrt(3));
+
+            Console.WriteLine($"Volume of Pyramid = {result}");
+        }
+    }
+}
---
+++ TETriangle.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace labrab
+{
+    class TETriangle
+    {
+        private uint _edgeLength;
+        public uint EdgeLength
+        {
+            get => _edgeLength;
+            set
+            {
+                _edgeLength = value;
+            }
+        }
+
+        public TETriangle()
+        {
+
+        }
+        public TETriangle(uint edgeLength)
+        {
+            EdgeLength = edgeLength;
+        }
+        public TETriangle(TETriangle triangle)
+        {
+            EdgeLength = triangle.EdgeLength;
+        }
+
+        public override string ToString()
+        {
+            return "TETriangle, EdgeLenght = " + EdgeLength;
+        }
+
+        public virtual void OutputFields()
+        {
+            Console.WriteLine($"Edge Length = {EdgeLength}");
+        }
+        public virtual void InputFields()
+        {
+            bool isWrong = true;
+            uint tmp;
+
+            // Check for incorrect numbers
+            while(isWrong)
+            {
+                Console.WriteLine("Input Edge Length:");
+                tmp = Convert.ToUInt32(Console.ReadLine());
+
+                if (tmp > 0)
+                {
+                    EdgeLength = tmp;
+                    isWrong = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect number. Try again. ");
+                }
+            }
+        }
+
+        
+        public void FindArea()
+        {
+            double result;
+            // S = (√3 / 4) * pow(a,2)
+            result = (Math.Sqrt(3) / 4) * Math.Pow(EdgeLength, 2);
+            Console.WriteLine($"Area of Triangle = {result}");
+        }
+        public void FindPerimeter()
+        {
+            double result;
+            result = Math.Pow(EdgeLength, 3);
+            Console.WriteLine($"Perimeter of Triangle = {result}");
+        }
+
+        public void ComparisonWith(TETriangle triangle)
+        {
+            if(EdgeLength == triangle.EdgeLength)
+                Console.WriteLine("The triangles are equal");
+            else
+                Console.WriteLine("The triangles aren't equal");
+        }
+
+        public static uint operator *(TETriangle triangle, uint number)
+        {
+            return triangle.EdgeLength * number;
+        }
+        public static uint operator *(uint number, TETriangle triangle)
+        {
+            return number * triangle.EdgeLength;
+        }
+    }
+}
